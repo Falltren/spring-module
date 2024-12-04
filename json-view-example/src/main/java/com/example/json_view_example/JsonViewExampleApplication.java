0f2f6facaeab3ed9cc3bb7359b8a75169058d22c
falltren <aaguzey@gmail.com>
2024-12-04 16:40:26 +1000

    init

---
+++ json-view-example/src/main/java/com/example/json_view_example/JsonViewExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.json_view_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonViewExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonViewExampleApplication.class, args);
+	}
+
+}
---
+++ json-view-example/src/test/java/com/example/json_view_example/JsonViewExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.json_view_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonViewExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
