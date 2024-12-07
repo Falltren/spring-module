edd4382f3e72a95fe3c632a892cbf7d5e2773189
falltren <aaguzey@gmail.com>
2024-12-07 11:29:43 +1000

    init objectmapper project

---
+++ objectmapper-example/src/main/java/com/fallt/objectmapper_example/ObjectmapperExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fallt.objectmapper_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectmapperExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectmapperExampleApplication.class, args);
+	}
+
+}
---
+++ objectmapper-example/src/test/java/com/fallt/objectmapper_example/ObjectmapperExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallt.objectmapper_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectmapperExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
